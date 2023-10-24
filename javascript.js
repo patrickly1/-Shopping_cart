4c3337f9544a57b9fb3d31c795a51e61bd03adeb
patrickly1 <112747551+patrickly1@users.noreply.github.com>
2023-10-24 12:23:20 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const listElement = document.querySelector("ul");
+const ulElement = document.querySelector("ul");
 const inputElement = document.querySelector("input");
 const buttonElement = document.querySelector("button");
 
@@ -6,6 +6,24 @@ function buttonClick() {
     const inputValue = inputElement.value;
 
     inputElement.value = "";
+
+    const liElement = document.createElement("li");
+    const spanElement = document.createElement("span");
+    const newButtonElement = document.createElement("button")
+
+    liElement.appendChild(spanElement);
+    liElement.appendChild(newButtonElement);
+
+    spanElement.textContent = inputValue;
+    newButtonElement.textContent = "Delete";
+
+    ulElement.appendChild(liElement);
+
+    newButtonElement.addEventListener("click", function (e) {
+        liElement.remove();
+    });
+
+    inputElement.focus();
 }
 
 buttonElement.addEventListener("click", buttonClick);
